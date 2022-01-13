518d6164eab5f37cbb6116462cf0aec708c7cd0b
Mauro Roda <Mauro@Mauro-PC.(mauro)>
2022-01-13 11:36:46 -0300

    adiciona função

--- Projeto 2/lampada.js
+++ Projeto 2/lampada.js
@@ -3,20 +3,32 @@ const turnOFF = document.getElementById('turnOFF');
 const lamp = document.getElementById ('lampada');
 const trocaLamp = document.getElementById ('trocarLamp');
 
+function lampQuebrada(){
+    return lamp.src.indexOf ('quebrada') >-1
+}
+
 function  lampOn(){
+    if (!lampQuebrada ()){
     lamp.src = './img/acesa.jpg';
+    }
 }
 
 function lampOFF(){
+    if (!lampQuebrada ()){
     lamp.src = './img/apagada.jpg';
+    }
 }
 
 function quebrada(){
     lamp.src = './img/quebrada.jpg';
 }
 
+function trocalamp (){
+    lamp.src = './img/apagada.jpg';
+}
+
 turnOn.addEventListener('click', lampOn);
 turnOFF.addEventListener('click', lampOFF);
 lampada.addEventListener('dblclick', quebrada);
 lampada.addEventListener('mouseover',lampOn);
-trocarLamp.addEventListener('click', lampOFF);
+trocarLamp.addEventListener('click', trocalamp);
